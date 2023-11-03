a41344089f27c4c663e2252bbe57d4b8345e6da6
Pravin1761 <145894932+Pravin1761@users.noreply.github.com>
2023-11-04 02:59:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@
     key_name     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCC6NIELlr1nZN0idmDjafC216EmRtQpaFDFcfuqPcGPIugzTSvYRau4fBsiuNWxySLnAhkcrpxPDZxku4GK2MpaN2IMxHTgys4wZljknWyIrJGRlZbLwG3yx1acIT1qQVhYy9XW8maIb5EmVX+VhwC3p/x/oxNtTtJSUzmGmuvekWD1WxYTUQlVFLymCkeA0tFJkqImfGWgVN+42pT8T8/YbQKymMT7mFzb04U1WhmZrqOJ6+Niiv74NiK7Wda9EPi6tha6A8wVIA536pWb7v9EeFu5LSQx1dPHZtzE+iCgcSjsxfcWs+hcy7IOCllfwKqPRfYh24LaHYJvx310bXN imported-openssh-key"
 
     tags = {
-      Name = "CentOS Instance"
+      Name = "Redhat Instance"
     }
   }
