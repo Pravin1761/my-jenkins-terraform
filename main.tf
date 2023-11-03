4c93771d0aeade75529cad193d746c94774ada1f
Pravin1761 <145894932+Pravin1761@users.noreply.github.com>
2023-11-04 03:07:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
   resource "aws_instance" "ubuntu" {
     ami          = "ami-0e83be366243f524a"
     instance_type = "t2.micro"
-    key_name     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCC6NIELlr1nZN0idmDjafC216EmRtQpaFDFcfuqPcGPIugzTSvYRau4fBsiuNWxySLnAhkcrpxPDZxku4GK2MpaN2IMxHTgys4wZljknWyIrJGRlZbLwG3yx1acIT1qQVhYy9XW8maIb5EmVX+VhwC3p/x/oxNtTtJSUzmGmuvekWD1WxYTUQlVFLymCkeA0tFJkqImfGWgVN+42pT8T8/YbQKymMT7mFzb04U1WhmZrqOJ6+Niiv74NiK7Wda9EPi6tha6A8wVIA536pWb7v9EeFu5LSQx1dPHZtzE+iCgcSjsxfcWs+hcy7IOCllfwKqPRfYh24LaHYJvx310bXN imported-openssh-key"
+    key_name     = "my-jenkins"
 
     tags = {
       Name = "Ubuntu Instance"
@@ -11,7 +11,7 @@
   resource "aws_instance" "centos" {
     ami          = "ami-0931978297f275f71"
     instance_type = "t2.micro"
-    key_name     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCC6NIELlr1nZN0idmDjafC216EmRtQpaFDFcfuqPcGPIugzTSvYRau4fBsiuNWxySLnAhkcrpxPDZxku4GK2MpaN2IMxHTgys4wZljknWyIrJGRlZbLwG3yx1acIT1qQVhYy9XW8maIb5EmVX+VhwC3p/x/oxNtTtJSUzmGmuvekWD1WxYTUQlVFLymCkeA0tFJkqImfGWgVN+42pT8T8/YbQKymMT7mFzb04U1WhmZrqOJ6+Niiv74NiK7Wda9EPi6tha6A8wVIA536pWb7v9EeFu5LSQx1dPHZtzE+iCgcSjsxfcWs+hcy7IOCllfwKqPRfYh24LaHYJvx310bXN imported-openssh-key"
+    key_name     = "my-jenkins"
 
     tags = {
       Name = "Redhat Instance"
